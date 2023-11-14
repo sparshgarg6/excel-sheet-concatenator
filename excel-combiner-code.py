9752150db543cb62da19032696a6f9da6a152029
sparshgarg6 <63985149+sparshgarg6@users.noreply.github.com>
2023-11-14 11:42:51 -0800

    Update excel-combiner-code.py

--- excel-combiner-code.py
+++ excel-combiner-code.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-df = pd.concat(pd.read_excel(r'/Users/sparshgarg/Downloads/FILE_5721.xlsx', skiprows=[0], sheet_name = None), ignore_index=True)
+df = pd.concat(pd.read_excel(r'/Users/sparshgarg/Downloads/FILE_5721.xlsx', skiprows=[0], sheet_name = None), ignore_index=True) #replace this path with the path of your original file.
 
-writer = pd.ExcelWriter('new_file.xlsx')
+writer = pd.ExcelWriter('new_file.xlsx') #name of the new file
 df.to_excel(writer, sheet_name='my_analysis', index=False, na_rep='NaN')
 for column in df:
     column_width = max(df[column].astype(str).map(len).max(), len(column))
